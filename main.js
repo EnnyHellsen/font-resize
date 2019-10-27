58a6a31c0737d3910b05c35388b8881710b34bd1
EnnyHellsen <enny.hellsen@weahead.se>
2019-10-27 14:53:53 +0100

    store input values after browser refresh

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const div = document.querySelector('.output-div');
 const textInput = document.querySelector('.input-text');
 const slider = document.querySelector('.slider');
 
+// Get localstorage values
+const previousTextValue = localStorage.getItem('text');
+const previousSliderValue = localStorage.getItem('slider');
+
+// Set input fields with previous values
+div.innerHTML = previousTextValue;
+textInput.value = previousTextValue;
+slider.value = previousSliderValue;
+div.style.width = previousSliderValue + 'vw';
+
 // Get the height of the output div as an integer
 const divHeight = parseInt(window.getComputedStyle(div).getPropertyValue('height'));
 
@@ -27,6 +37,10 @@ const updateText = () => {
   div.style.width = slider.value + 'vw';
   div.innerHTML = textInput.value;
   doFit();
+
+  // save values to access on browser refresh
+  localStorage.setItem('slider', slider.value);
+  localStorage.setItem('text', textInput.value);
 }
 
 slider.addEventListener('input', updateText);
